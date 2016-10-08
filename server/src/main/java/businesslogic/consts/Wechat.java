772aa68fa41b6e4b7f79734e03a1aa688519a2da
wjr14 <wjr14@software.nju.edu.cn>
2016-10-09 01:20:05 +0800

    知乎版

---
+++ server/src/main/java/businesslogic/consts/Wechat.java
@@ -0,0 +1,49 @@
+package businesslogic.consts;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * 微信版本
+ * Created by Sissel on 2016/10/8.
+ */
+public class Wechat extends Vendor
+{
+    private static final String search_0_ID = "sogou_vr_11002601_title_0";
+    private static final String img_0_ID = "sogou_vr_11002601_img_0";
+    private static final String title_ID = "activity-name";
+    private static final String mainBody_ID = "js_content";
+    private static final String search_template = "http://weixin.sogou.com/weixin?type=2&query={key_word}";
+
+    @Override
+    public String getSearch_0_ID()
+    {
+        return search_0_ID;
+    }
+
+    @Override
+    public String getImg_0_ID()
+    {
+        return img_0_ID;
+    }
+
+    @Override
+    public String getSearchTemplate()
+    {
+        return search_template;
+    }
+
+    @Override
+    public String getTitle(Document document)
+    {
+        Element titleElement = document.getElementById(title_ID);
+        return titleElement.ownText();
+    }
+
+    @Override
+    public String getMainBody(String uri, Document document)
+    {
+        Element mainBody = document.getElementById(mainBody_ID);
+        return mainBody.toString();
+    }
+}
--- server/src/main/java/businesslogic/webCrawler.java
+++ server/src/main/java/businesslogic/webCrawler.java
@@ -1,5 +1,8 @@
 package businesslogic;
 
+import businesslogic.consts.Vendor;
+import businesslogic.consts.Wechat;
+import businesslogic.consts.Zhihu;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,10 +15,10 @@ import org.springframework.web.client.RestTemplate;
  */
 public class webCrawler
 {
-    public static JSONObject searchInSogou(String keyWords)
+    public static JSONObject searchInSogou(String keyWords, Vendor vendor)
     {
         RestTemplate rt = new RestTemplate();
-        String url = "http://weixin.sogou.com/weixin?type=2&query={key_word}";
+        String url = vendor.getSearchTemplate();
 
         String searchResult = rt.getForObject(url, String.class, keyWords);
 
@@ -23,13 +26,13 @@ public class webCrawler
 
         Document searchResultDoc = Jsoup.parse(searchResult);
 
-        String contentUri = searchResultDoc.getElementById("sogou_vr_11002601_title_0").attr("href");
+        String contentUri = searchResultDoc.getElementById(vendor.getSearch_0_ID()).attr("href");
         String content = new RestTemplate().getForObject(contentUri, String.class);
 
         Document contentDoc = Jsoup.parse(content);
-        String pictureUri = extractPicture(searchResult);
-        String title = extractTitle(contentDoc);
-        String mainBody = extractMainBody(contentDoc);
+        String pictureUri = extractPicture(searchResult, vendor);
+        String title = vendor.getTitle(contentDoc);
+        String mainBody = vendor.getMainBody(contentUri, contentDoc);
 
         json.put("title", title)
                 .put("content", mainBody)
@@ -39,31 +42,19 @@ public class webCrawler
         return json;
     }
 
-    private static String extractPicture(String html)
+    private static String extractPicture(String html, Vendor vendor)
     {
         Document doc = Jsoup.parse(html);
 
-        Element picFather = doc.getElementById("sogou_vr_11002601_img_0");
+        Element picFather = doc.getElementById(vendor.getImg_0_ID());
         Element pic = picFather.child(0);
         String pictureUri = pic.attr("src");
 
         return pictureUri;
     }
 
-    private static String extractMainBody(Document contentDoc)
-    {
-        Element mainBody = contentDoc.getElementById("js_content");
-        return mainBody.toString();
-    }
-
-    private static String extractTitle(Document contentDoc)
-    {
-        Element titleElement = contentDoc.getElementById("activity-name");
-        return titleElement.ownText();
-    }
-
     public static void main(String[] args)
     {
-        System.out.println(searchInSogou("我们都老了"));
+        System.out.println(searchInSogou("我们都老了", new Zhihu()));
     }
 }
