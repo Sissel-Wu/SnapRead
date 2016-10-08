815acfe699554a85561adc08ae95432ebdf64a62
wjr14 <wjr14@software.nju.edu.cn>
2016-10-08 20:56:02 +0800

    刚刚是去跟踪，这个才是代码

--- server/src/main/java/businesslogic/webCrawler.java
+++ server/src/main/java/businesslogic/webCrawler.java
@@ -1,6 +1,9 @@
 package businesslogic;
 
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -9,19 +12,58 @@ import org.springframework.web.client.RestTemplate;
  */
 public class webCrawler
 {
-    public static void searchInSogou(String keyWords)
+    public static JSONObject searchInSogou(String keyWords)
     {
         RestTemplate rt = new RestTemplate();
         String url = "http://weixin.sogou.com/weixin?type=2&query={key_word}";
-        String result = rt.getForObject(url, String.class, keyWords);
 
+        String searchResult = rt.getForObject(url, String.class, keyWords);
 
+        JSONObject json = new JSONObject();
 
-        System.out.println(result);
+        Document searchResultDoc = Jsoup.parse(searchResult);
+
+        String contentUri = searchResultDoc.getElementById("sogou_vr_11002601_title_0").attr("href");
+        String content = new RestTemplate().getForObject(contentUri, String.class);
+
+        Document contentDoc = Jsoup.parse(content);
+        String pictureUri = extractPicture(searchResult);
+        String title = extractTitle(contentDoc);
+        String mainBody = extractMainBody(contentDoc);
+
+        json.put("title", title)
+                .put("content", mainBody)
+                .put("post_url", contentUri)
+                .put("post_img", pictureUri);
+
+        return json;
+    }
+
+    private static String extractPicture(String html)
+    {
+        Document doc = Jsoup.parse(html);
+
+        Element picFather = doc.getElementById("sogou_vr_11002601_img_0");
+        Element pic = picFather.child(0);
+        String pictureUri = pic.attr("src");
+
+        return pictureUri;
+    }
+
+    private static String extractMainBody(Document contentDoc)
+    {
+        Element mainBody = contentDoc.getElementById("js_content");
+        return mainBody.toString();
+    }
+
+    private static String extractTitle(Document contentDoc)
+    {
+        Element titleElement = contentDoc.getElementById("activity-name");
+        return titleElement.ownText();
     }
 
     public static void main(String[] args)
     {
-        searchInSogou("小道消息");
+        System.out.println(searchInSogou("我们都老了"));
     }
 }
